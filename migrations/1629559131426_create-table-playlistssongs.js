f44c7eea236499c894ee68ffa3932fee9c1b2f81
mnfaizp <zynthkas@gmail.com>
2021-08-26 15:29:33 +0700

    migration and bcrypt

---
+++ migrations/1629559131426_create-table-playlistssongs.js
@@ -0,0 +1,29 @@
+/* eslint-disable camelcase */
+
+exports.shorthands = undefined;
+
+exports.up = (pgm) => {
+  pgm.createTable('playlistssongs', {
+    id: {
+      type: 'VARCHAR(50)',
+      primaryKey: true,
+    },
+    playlist_id: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+    },
+    song_id: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+    },
+  });
+
+  pgm.addConstraint('playlistssongs', 'fk_playlistssongs.song_id_songs.id', 'FOREIGN KEY(song_id) REFERENCES songs(id) ON DELETE CASCADE');
+  pgm.addConstraint('playlistssongs', 'fk_playlistssongs.playlist_id_songs.id', 'FOREIGN KEY(playlist_id) REFERENCES playlists(id) ON DELETE CASCADE');
+
+  pgm.addConstraint('playlistssongs', 'unique_song_id_and_playlist_id', 'UNIQUE(song_id, playlist_id)');
+};
+
+exports.down = (pgm) => {
+  pgm.createTable('playlistsSongs');
+};
